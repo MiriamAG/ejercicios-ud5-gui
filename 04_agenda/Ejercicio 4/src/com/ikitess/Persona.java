c40533df4028a376e02fb3aef8ac67bb02ac49b3
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2017-03-23 20:31:55 +0100

    Agenda de Iñaki y Carlos

---
+++ 04_agenda/Ejercicio 4/src/com/ikitess/Persona.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ikitess;
+
+/**
+ *
+ * @author ikitess
+ */
+class Persona {
+
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + '\n' + "Teléfono: " + telefono + '\n' + "Email: " + email;
+    }
+
+    public Persona(String nombre, String telefono, String email) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
